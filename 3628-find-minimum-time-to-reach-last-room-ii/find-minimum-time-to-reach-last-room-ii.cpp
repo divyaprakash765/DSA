12179dec41b4d5cffbbf829c8e5609c0e65a2687
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-05-10 18:36:34 +0530

    Time: 1069 ms (21.78%) | Memory: 168 MB (27.32%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size(), m = moveTime[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minh;
-        minh.push({0, 0, 0});    
-        moveTime[0][0] = 0;
-
-        vector<vector<int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        while (!minh.empty()) {
-            auto current = minh.top();
-            minh.pop();
-            int currTime = current[0];
-            int currRow  = current[1];
-            int currCol  = current[2];
-            if (currTime >= dp[currRow][currCol]) continue;
-            if (currRow == n - 1 && currCol == m - 1) return currTime;
-            dp[currRow][currCol] = currTime;
-
-            for (auto& direction : directions) {
-                int nextRow = currRow + direction[0];
-                int nextCol = currCol + direction[1];
-                if (nextRow >= 0 && nextRow < n &&
-                    nextCol >= 0 && nextCol < m &&
-                    dp[nextRow][nextCol] == INT_MAX) {
-                    int cost = (currRow + currCol) % 2 + 1; 
-                    int start = max(moveTime[nextRow][nextCol], currTime);
-                    int nextTime = start + cost;
-                    minh.push({nextTime, nextRow, nextCol});
-                }
-            }
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        
+        vector<vector<int>> arr_time(n,vector<int>(m,INT_MAX));
+        arr_time[0][0] = 0;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0});
+        bool temp = true;
+        while(!pq.empty()){
+            int time = pq.top()[0];
+            int i = pq.top()[1];
+            int j = pq.top()[2];
+            pq.pop();
+        for(auto& d : dir){
+            int i_ = i + d[0];
+            int j_ = j + d[1];
+            if(i_ < 0 || j_ < 0 || i_ >= n || j_ >= m) continue;
+            int num = (i+j)%2 + 1;
+            int final_time = max(time,moveTime[i_][j_]) + num;
+            if(arr_time[i_][j_] > final_time){
+                arr_time[i_][j_] = final_time;
+                pq.push({final_time,i_,j_});
+            } 
         }
-        return -1;
+        }
+
+        return arr_time[n-1][m-1];
+
     }
 };
