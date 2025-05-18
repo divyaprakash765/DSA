ddca93d01a4994c10c3b6847a36f1441bb1416c2
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-05-18 14:02:53 +0530

    Time: 111 ms (17.54%) | Memory: 34.5 MB (32.21%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.cpp
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    bool differByOneChar(string word1, string word2) {
-        if (word1.length() != word2.length()) return false;
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++) 
-            diffCount += word1[i] != word2[i];
-        return diffCount == 1;
+    bool hamming_dist(string a,string b){
+        if(a.size() != b.size()) return false;
+        int cnt = 0;
+        for(int i=0;i<a.size();i++){
+            if(a[i] != b[i]) cnt++;
+            if(cnt > 1) return false; 
+        }
+        return cnt == 1;
     }
-    
+
     vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        int n = groups.size();
-        vector<int> dp(n, 1), parent(n, -1);
-        int maxi = 0;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (groups[i] != groups[j] && 
-                        differByOneChar(words[i], words[j]) && 
-                            dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
-                }
-            }
-            maxi = max(maxi, dp[i]);
-        }
-        
-        vector<string> result;
-        for (int i = 0; i < n; i++) {
-            if (maxi == dp[i]) {
-                while (i != -1) {
-                    result.push_back(words[i]);
-                    i = parent[i];
-                }
-                break;
+        int n = words.size();
+        vector<int> dp(n,1);
+        vector<int> hash(n,1);
+
+        for(int i=0;i<n;i++){
+            hash[i] = i;
+        for(int prev_idx = 0;prev_idx <= i-1;prev_idx++){
+            if((hamming_dist(words[i],words[prev_idx])) && groups[i] != groups[prev_idx] && 1 + dp[prev_idx] > dp[i]){
+                dp[i] = 1 + dp[prev_idx];
+                hash[i] = prev_idx;
             }
         }
-        reverse(result.begin(), result.end());
-        return result;
+        }
+
+        int size = -1;
+        int last_idx = -1;
+
+        for(int i=0;i<n;i++){
+            if(dp[i] > size){
+                size = dp[i];
+                last_idx = i;
+            }
+        }
+        vector<string> ans;
+        ans.push_back(words[last_idx]);
+
+        while(last_idx != hash[last_idx]){
+            last_idx = hash[last_idx];
+            ans.push_back(words[last_idx]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
