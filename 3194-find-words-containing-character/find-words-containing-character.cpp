339036200971db1865bf2ccb21c9d28a3632e048
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-05-24 10:56:10 +0530

    Time: 3 ms (26.74%) | Memory: 33 MB (56.49%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -4,12 +4,7 @@ public:
         vector<int> ans;
         int i = 0;
         for(auto& word:words){
-         for(int j=0;j<word.size();j++){
-            if(word[j] == x){
-                ans.push_back(i);
-                break;
-            }
-         }
+         if(word.find(x) != string::npos) ans.push_back(i);
           i++;
         }
         return ans;
