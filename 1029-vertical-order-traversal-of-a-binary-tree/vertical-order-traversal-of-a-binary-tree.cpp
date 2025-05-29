3b08d3d2164488da949c9a77531ba453768252b1
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-05-29 20:31:36 +0530

    Time: 4 ms (22.94%) | Memory: 16.1 MB (57.29%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        if (!root) return ans;
+        if(!root) return ans;
 
-        // queue of (node, (vertical, level))
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        // map: vertical -> level -> multiset of node values
-        map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*,pair<int,int>>> q; //(node,vertical,level)
 
-        q.push({root, {0, 0}});
+        map<int,map<int,multiset<int>>> mp;
 
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            TreeNode* node = it.first;
-            int vert = it.second.first;
-            int level = it.second.second;
+        q.push({root,{0,0}});
 
-            mp[vert][level].insert(node->val);
+        while(!q.empty()){
 
-            if (node->left)
-                q.push({node->left, {vert - 1, level + 1}});
-            if (node->right)
-                q.push({node->right, {vert + 1, level + 1}});
+          auto it = q.front();
+          q.pop();
+          TreeNode* node = it.first;
+          int vert = it.second.first;
+          int level = it.second.second;
+          mp[vert][level].insert(node->val);
+          if(node->left){
+            q.push({node->left, {vert - 1, level + 1}});
+          }
+          if(node->right){
+            q.push({node->right,{vert+1,level+1}});
+          } 
         }
-
-        for (auto& p : mp) {
+        for(auto& p : mp){
             vector<int> col;
-            for (auto& q : p.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
+            for(auto q: p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
             }
             ans.push_back(col);
         }
-
         return ans;
     }
 };
