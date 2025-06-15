ccf85a18055cadb274f770acff73a218a56e2748
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-06-15 19:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (93.87%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        long long n = nums.size();
-        set<long long> s(nums.begin(),nums.end());
-        long long mini = *min_element(nums.begin(),nums.end());
-        long long maxi = *max_element(nums.begin(),nums.end());
-        long long ans;
-        if(mini > 1) ans = mini-1;
-        else ans = maxi+1;
-        for(long long i=mini;i<=maxi;i++){
-            if(i <= 0) continue;
-            if(s.find(i) == s.end()){
-                ans = i;
-                break;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;
             }
         }
-        if(mini > 1 || maxi < 1) ans = 1;
-        return ans;
+
+        for (int i = 0; i < n; i++) {
+            int num = abs(nums[i]);
+            if (num <= n) {
+                int idx = num - 1;
+                if (nums[idx] > 0) {
+                    nums[idx] = -nums[idx];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) return i + 1;
+        }
+
+        return n + 1;
     }
 };
+
