bb23fc2f3c29b456d5a59ebd1fafaa91e06f9f9c
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-06-15 16:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (69.19%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -2,45 +2,49 @@ class Solution {
 public:
     int maxDiff(int num) {
         string s = to_string(num);
-        string maxi = s, mini = s;
+        int n = s.size();
 
-        // ---- MAXIMIZE: Replace first digit != 9 with 9 ----
-        char toReplaceMax = 0;
-        for (char c : s) {
-            if (c != '9') {
-                toReplaceMax = c;
-                break;
-            }
-        }
-        if (toReplaceMax) {
-            for (char& c : maxi) {
-                if (c == toReplaceMax) c = '9';
-            }
+        int x = s[0]-'0';
+        string maxi = "";
+        
+        int i = 0;
+        while(i < n && x == 9){
+            maxi += s[i];
+            i++;
+           if(i != n) x = s[i]-'0';
         }
 
-        // ---- MINIMIZE ----
-        // Case 1: First digit != 1 → replace it with 1
-        char toReplaceMin = 0;
-        if (s[0] != '1') {
-            toReplaceMin = s[0];
-            for (char& c : mini) {
-                if (c == toReplaceMin) c = '1';
+        while(i < n){
+            if(s[i]-'0' == x){
+                maxi += '9';
+            } else{
+                maxi += s[i];
             }
-        } else {
-            // Case 2: Replace first digit (after index 0) not 0 or 1 → with 0
-            for (int i = 1; i < s.size(); ++i) {
-                if (s[i] != '0' && s[i] != '1') {
-                    toReplaceMin = s[i];
-                    break;
-                }
+            i++;
+        }
+
+        string mini = "";
+
+        x = s[0]-'0';
+        i = 0;
+
+        while(i < n && (x == 1 || x == 0)){
+            mini += s[i];
+            i++;
+            if(i != n) x = s[i]-'0';
+        }
+        char replace = '1';
+        if(i != 0) replace = '0';
+        while(i < n){
+            if(s[i]-'0' == x){
+                mini += replace;
+            } else{
+                mini += s[i];
             }
-            if (toReplaceMin) {
-                for (int i = 1; i < mini.size(); ++i) {
-                    if (mini[i] == toReplaceMin) mini[i] = '0';
-                }
-            }
+            i++;
         }
 
         return stoi(maxi) - stoi(mini);
+
     }
 };
