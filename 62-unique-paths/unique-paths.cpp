7cfe9798ca51dc393b6bcb4433d50f42c620e954
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-06-20 00:17:53 +0530

    Time: 1 ms (24.97%) | Memory: 9.1 MB (68.40%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -16,7 +16,20 @@ public:
 
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(0,0,m,n,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        dp[0][0] = 1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i == 0 && j == 0) continue;
+                int up = 0;
+                int left = 0;
+                if(i-1 >= 0) up = dp[i-1][j];
+                if(j-1 >= 0) left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
