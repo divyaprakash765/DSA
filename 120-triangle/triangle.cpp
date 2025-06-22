59a3260954db400a854c5423574411aa4bbcddca
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-06-22 19:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (51.88%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -15,8 +15,20 @@ public:
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        
-        return f(0,0,triangle,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        dp[0][0] = triangle[0][0];
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+             int up = INT_MAX, leftUp = INT_MAX;
+
+            if(j < i) up = dp[i-1][j];
+            if(j > 0) leftUp = dp[i-1][j-1];
+
+            dp[i][j] = triangle[i][j] + min(up, leftUp);
+            }
+        }
+
+        return *min_element(dp[n-1].begin(), dp[n-1].begin() + n);
     }
 };
