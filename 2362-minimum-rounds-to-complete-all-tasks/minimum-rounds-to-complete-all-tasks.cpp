3d4a90448e6218c88d14450eaca1613b16acc4e3
divyaprakash765 <2022ugce092@nitjsr.ac.in>
2025-08-25 00:44:38 +0530

    Time: 47 ms (21.69%) | Memory: 107.5 MB (74.44%) - LeetSync

---
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        int n = tasks.size();
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            mp[tasks[i]]++;
+        }
+        int ans = 0;
+        for(auto it : mp){
+            int freq = it.second;
+            if(freq == 1) return -1;
+            if(freq % 3 == 0){
+                ans += freq/3;
+            } else {
+                ans += (freq/3) + 1;
+            }
+        }
+
+        return ans;
+    }
+};
